39bf821029696c6eeeac7d19bcf6455b84be91a4
Branko Bogdanovic <branko.bogd@gmail.com>
2018-08-25 22:02:02 +0200

    Create script for controling the vehicles. Implemented simple accerelation and breaking.

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarControl : MonoBehaviour {
+public class CarControl : MonoBehaviour
+{
 
     [Header("Wheels")]
     public WheelCollider FrontLeftWheel;
@@ -10,12 +11,31 @@ public class CarControl : MonoBehaviour {
     public WheelCollider BackLeftWheel;
     public WheelCollider BackRightWheel;
 
+    [Header("Constants")]
+    public float TotalDriveTorque = 600f;
+    public float BrakeTorque = 12000f;
+    public float TopSpeed = 20f;
+
+
     void Start()
     {
-        
-    }	
-    
-    void Update () {
-        
     }
+
+    void Update()
+    {
+
+        float accelerationInput = Mathf.Clamp(Input.GetAxis("Vertical"), -1, 1);
+
+        var driveTorque = accelerationInput * TotalDriveTorque;
+
+        var relative = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+        Debug.Log(relative);
+
+        FrontLeftWheel.brakeTorque = 0f;
+        FrontRightWheel.brakeTorque = 0f;
+
+        FrontLeftWheel.motorTorque = driveTorque;
+        FrontRightWheel.motorTorque = driveTorque;
+    }
+
 }
