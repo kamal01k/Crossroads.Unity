e53b0fbb787bf5f965f912b8ba4b5462ee61bbe8
Branko Bogdanovic <branko.bogd@gmail.com>
2018-08-25 14:01:51 +0200

    Add initial car control script

---
+++ Assets/Scripts/CarControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour {
+
+    [Header("Wheels")]
+    public WheelCollider FrontLeftWheel;
+    public WheelCollider FrontRightWheel;
+    public WheelCollider BackLeftWheel;
+    public WheelCollider BackRightWheel;
+
+    void Start()
+    {
+        
+    }	
+    
+    void Update () {
+        
+    }
+}
